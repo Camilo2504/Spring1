d1512883b537a28c166ef2174d1671bcebbd26f8
JhonatanChavarroarias <110508336+JhonatanChavarroArias@users.noreply.github.com>
2022-10-16 17:24:09 -0500

    Primer Comentario Jhonatan Chavarro Arias

---
+++ Sprint1.java
@@ -0,0 +1,11 @@
+public class Sprint1 {
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        System.out.println("Comentario Jhonatan Chavarro Arias");
+
+    }
+
+}
